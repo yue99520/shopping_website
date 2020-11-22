efa53741855e67763ede86387d11c782bcfef525
ernie <dy99520@gmail.com>
2020-11-22 13:49:14 +0800

    [dev] create AdminSeeder

---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Carbon\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => Hash::make('admin12345')
+        ]);
+    }
+}
